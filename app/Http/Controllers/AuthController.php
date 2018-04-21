5cf1c46368193700ea3304752b544e1c2979a890
Raca <araca.prasetyo@gmail.com>
2018-04-21 13:13:28 +0700

    move user to folder model & add login controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\UjianSiswa;
+use App\Models\User;
+use Carbon\Carbon;
+use File;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+    
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['username' => $request->input('username'),
+        'password' => $request->input('password')])){
+            return redirect('/home');
+        }
+        return redirect('/')->with('error','User atau password salah');
+    }
+
+    public function home()
+    {
+        $data['user'] = Auth::user();
+        switch(Auth::user()->level){
+            case 'admin':
+                return view('pages.admin.index', $data);
+                break;
+            case 'kajur':
+                return view('pages.guru.index', $data);
+                break;
+            case 'use':
+                return view('pages.proktor.index', $data);
+                break;
+            default:
+                Auth::logout();
+                return redirect('/')->with('error','User tidak dikenali');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $user = Auth::User();
+        Auth::logout();
+    }
+}
