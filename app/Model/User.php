f1715d19e18f3a06bd862a811e26d9cdec769f1c
Raca <araca.prasetyo@gmail.com>
2018-04-21 14:31:40 +0700

    add auth & fix model and db

--- app/Model/User.php
+++ app/Model/User.php
@@ -10,12 +10,20 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+    
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username','role','name', 'no_hp', 'email', 'line',
     ];
 
     /**
