8189ec38f8a9a69320acd9efda32f7be99d74dbd
Rina Wijaya <rinawkw@gmail.com>
2018-04-22 10:48:36 +0700

    Auto stash before merge of "development" and "origin/development"
    login regis

--- resources/views/user/signup.blade.php
+++ resources/views/user/signup.blade.php
@@ -3,47 +3,16 @@
 
 @include('layout.head')
 
-<body class="signup-page ">
-    <nav class="navbar  navbar-transparent    navbar-absolute  navbar-expand-lg " color-on-scroll="100" id="sectionsNav">
-        <div class="container">
-            <div class="navbar-translate">
-                <!-- <a class="navbar-brand" href="../index.html"> </a> -->She Needs Lab
-                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    <span class="navbar-toggler-icon"></span>
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/hmtc_its" target="_blank" data-original-title="Follow us on Twitter">
-                            <i class="fa fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com/HMTCFTIf" target="_blank" data-original-title="Like us on Facebook">
-                            <i class="fa fa-facebook-square"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/hmtc_its" target="_blank" data-original-title="Follow us on Instagram">
-                            <i class="fa fa-instagram"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="page-header header-filter" filter-color="purple" style="background-image: url('http://localhost/sheneedslab/public/img/bg2.jpeg');"">
+<body class="signup-page " style="padding-top:5rem;background-image: url('http://localhost/sheneedslab/public/img/bg2.jpeg');">
+    <div class="page-header">
         <div class="container">
             <div class="row">
-                <div class="col-md-10 ml-auto mr-auto">
-                    <div class="card card-signup">
+                <div class="col-md-8 ml-auto mr-auto">
+                    <div class="card card-signup" style="padding-top:1rem;">
                         <h2 class="card-title text-center">Register</h2>
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-5 ml-auto">
+                                <div class="col-md-8 ml-auto mr-auto">
                                     @if (session('error'))
                                         <div class="alert alert-danger alert-dismissible" role="alert">
                                         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -52,60 +21,62 @@
                                     @endif
                                     <form class="form" method="POST" action="{{ url('/register') }}">
                                         {{csrf_field()}}
-                                        <div class="form-group">
+                                        <div class="">
                                             <div class="input-group">
-                                                <input type="text" class="form-control" placeholder="NRP" name="username">
+                                            <div class="input-group-prepend">
+                                                    <span class="input-group-text">
+                                                        <i class="material-icons">person</i>
+                                                    </span>
+                                                </div>
+                                                <input type="text" class="form-control" placeholder=" NRP" name="username">
                                             </div>
                                         </div>
-                                        <div class="form-group">
+                                        <div class="">
                                             <div class="input-group">
-                                                <input type="text" class="form-control" placeholder="Nama Lengkap" name="name">
+                                            <div class="input-group-prepend">
+                                                    <span class="input-group-text">
+                                                        <i class="material-icons">face</i>
+                                                    </span>
+                                                </div>
+                                                <input type="text" class="form-control" placeholder=" Nama Lengkap" name="name">
                                             </div>
                                         </div>
-                                        {{-- <div class="form-group">
-                                            <div class="input-group">
-                                                <input type="dropdown-menu" class="form-control" placeholder="Role">
-                                            </div>
-                                        </div> --}}
-                                </div>
-                                <div class="col-md-5 mr-auto">
-                                        <div class="form-group">
+        
+                                
+                                        <div class="">
                                             <div class="input-group">
                                                 <div class="input-group-prepend">
                                                     <span class="input-group-text">
                                                         <i class="material-icons">mail</i>
                                                     </span>
                                                 </div>
-                                                <input type="text" class="form-control" placeholder="Email" name="email">
+                                                <input type="text" class="form-control" placeholder=" Email" name="email">
                                             </div>
                                         </div>
-                                        <div class="form-group">
+                                        <div class="">
                                             <div class="input-group">
                                                 <div class="input-group-prepend">
                                                     <span class="input-group-text">
                                                         <i class="material-icons">phone</i>
                                                     </span>
                                                 </div>
-                                                <input type="text" class="form-control" placeholder="Nomor HP" name="no_hp">
+                                                <input type="text" class="form-control" placeholder=" Nomor HP" name="no_hp">
                                             </div>
                                         </div>
-                                        <div class="form-group">
+                                        <div class="">
                                             <div class="input-group">
                                                 <div class="input-group-prepend">
                                                     <span class="input-group-text">
                                                         <i class="material-icons">lock_outline</i>
                                                     </span>
                                                 </div>
-                                                <input type="password" placeholder="Password..." class="form-control" name="password" />
+                                                <input type="password" placeholder=" Password..." class="form-control" name="password" />
                                             </div>
                                         </div>
                                         <div class="text-center">
                                             <button type="SUBMIT" class="btn btn-primary btn-round">sign up</button>
                                         </div>
                                     </form>
-                                </div>
-
-                                <div class="col-md-4 mr-auto ml-auto">
                                     <br>
                                     <br>
                                         <div class="text-center">
@@ -118,39 +89,9 @@
                 </div>
             </div>
         </div>
-        <footer class="footer ">
-            <div class="container">
-                <nav class="pull-left">
-                    <ul>
-                        <li>
-                            <a href="http://presentation.creative-tim.com">
-                                About Us
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://blog.creative-tim.com">
-                                Blog
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.creative-tim.com/license">
-                                Licenses
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="copyright pull-right">
-                    &copy;
-                    <script>
-                        document.write(new Date().getFullYear())
-                    </script>, made with <i class="material-icons">favorite</i> by
-                    <a href="https://www.creative-tim.com" target="_blank">Admin LP</a>
-                </div>
-            </div>
-        </footer>
+        
     </div>
 
-@include('layout.footer')
 
 </body>
 
